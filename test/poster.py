5c2309761bce725b00e90b74cddca271a8457417
kevin <kevin@kevindeMacBook-Pro.local>
2014-05-22 21:45:22 +0800

    cp.

--- test/poster.py
+++ test/poster.py
@@ -3,9 +3,9 @@ import urllib2
 
 #query_args = {'q':'query string', 'foo':'bar'}
 #encoded_args = urllib.urlencode(query_args)
-parameters = {'devicename':'my raspberry pi', 'devicedescription':'sn number'};
+parameters = {'devicename':'my raspberry pi', 'devicedescription':'sn number'}
 
-encoded_args = urllib.urlencode(parameters);
+encoded_args = urllib.urlencode(parameters)
 url = 'http://localhost/mybackground/index.php/RemoteIPRegister/Index/registerIP'
 #url = 'http://www.neuseeker.com/getip.php'
 print urllib2.urlopen(url, encoded_args).read()
